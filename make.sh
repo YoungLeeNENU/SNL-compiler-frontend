a4631e3ce19f6dff6badf1e886f2802bf86f2312
YoungLee <YoungLeeNENU@gmail.com>
2013-01-01 23:03:36 +0800

    1-1-2013

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 # Compile the project
-python main.py bubble_sort.snl > syntactic_analysis.txt
-# pypy main.py bubble_sort.snl > syntactic_analysis.txt
+python python/main.py test_file/bubble_sort.snl > syntactic_analysis.txt
+# pypy python/main.py test_file/bubble_sort.snl > syntactic_analysis.txt
