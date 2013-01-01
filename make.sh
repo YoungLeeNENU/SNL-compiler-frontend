cb5119a30847471236344d74a27aea899d718dce
YoungLee <YoungLeeNENU@gmail.com>
2013-01-01 23:22:18 +0800

    compile the project

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 # Compile the project
-python python/main.py test_file/bubble_sort.snl > syntactic_analysis.txt
-# pypy python/main.py test_file/bubble_sort.snl > syntactic_analysis.txt
+python python/main.py test_file/bubble_sort.snl > syntactic_analysis
+# pypy python/main.py test_file/bubble_sort.snl > syntactic_analysis
